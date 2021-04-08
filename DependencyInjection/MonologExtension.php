61f32b692a9fb024e737c9d49abea2a90368291f
Thibault Buathier <thibault.buathier@gmail.com>
2021-04-08 11:01:42 +0200

    Adding exclude_fields in the configuration of slack handlers

--- DependencyInjection/MonologExtension.php
+++ DependencyInjection/MonologExtension.php
@@ -662,6 +662,7 @@ class MonologExtension extends Extension
                 $handler['bubble'],
                 $handler['use_short_attachment'],
                 $handler['include_extra'],
+                $handler['exclude_fields'],
             ]);
             if (isset($handler['timeout'])) {
                 $definition->addMethodCall('setTimeout', [$handler['timeout']]);
@@ -682,6 +683,7 @@ class MonologExtension extends Extension
                 $handler['include_extra'],
                 $handler['level'],
                 $handler['bubble'],
+                $handler['exclude_fields'],
             ]);
             break;
 
